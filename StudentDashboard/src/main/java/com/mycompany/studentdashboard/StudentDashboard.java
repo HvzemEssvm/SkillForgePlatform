49bac8434c26b7608b11d71749590b5ea9527850
nouraldenmohamed35-hub <nouraldenmohamed35@gmail.com>
2025-11-15 00:26:18 +0200

    cre Student Dashboard

---
+++ StudentDashboard/src/main/java/com/mycompany/studentdashboard/StudentDashboard.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.studentdashboard;
+
+/**
+ *
+ * @author afifi.store
+ */
+public class StudentDashboard {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
