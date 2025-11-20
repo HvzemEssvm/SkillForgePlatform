6c9fe5ae28d302a2def1b12e9244b4ff1e802c7b
Zeyad Hasan <studying.zezo@gmail.com>
2025-11-20 13:06:25 +0200

    add empty constructor in enrollment for jackson deserializing

--- SkillForge/src/main/java/com/mycompany/UserAccountManagement/Enrollment.java
+++ SkillForge/src/main/java/com/mycompany/UserAccountManagement/Enrollment.java
@@ -15,9 +15,14 @@ public class Enrollment {
     private String courseId;
     private ArrayList<String> completedLessons;
 
+    // Default constructor for Jackson deserialization
+    public Enrollment() {
+        this.completedLessons = new ArrayList<>();
+    }
+
     public Enrollment(String courseId, ArrayList<String> completedLessons) {
         this.courseId = courseId;
-        this.completedLessons = completedLessons;
+        this.completedLessons = completedLessons != null ? completedLessons : new ArrayList<>();
     }
 
     /**
