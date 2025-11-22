301b1722dd6141852e8e2d9f1ea318d4ac5321d9
nouraldenmohamed35-hub <nouraldenmohamed35@gmail.com>
2025-11-22 20:34:47 +0200

    add private Status status;

--- SkillForge/src/main/java/com/mycompany/CourseManagement/Course.java
+++ SkillForge/src/main/java/com/mycompany/CourseManagement/Course.java
@@ -17,7 +17,7 @@ public class Course {
     private String courseId;
     private String instructorId;
     private Status status;
-
+    private Status status;
     @JsonProperty(defaultValue = "[]")
     private ArrayList<String> studentIds;
 
